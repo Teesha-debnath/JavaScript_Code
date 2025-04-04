6f0a2adf774dbc6df48578a28ea77c7780f4fa91
teesha <debnathteesha51@gmail.com>
2025-04-04 05:44:33 +0000

    data

---
+++ basics/01_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat the code as newer version of js
+
+// alert("hello") this is not the correct syntax to be used in the vs code 
+//it has a newer syntax
+
+console.log(2+2) // code readability should be high
+
+let name = "teesha" //string
+let age = 20 //number
+let isLoggedIn = false //boolean
+let state = null;
+
+//object
+
+console.log(typeof "teesha"); //string
+console.log(typeof age); //number
+console.log(typeof null); //gives object
+console.log(typeof undefined); //gives undefined
+
+
+
---
+++ basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "teesha@123.com"
+var accountPassword = "12345"
+accountCity = "Bhubaneswar"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "teesha27@123.com"
+accountPassword = "2222"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
