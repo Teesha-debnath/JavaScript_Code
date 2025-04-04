cb8f5a49aed7699ac4b2503a82d049a5ffe0395e
teesha <debnathteesha51@gmail.com>
2025-04-04 06:29:47 +0000

    conversion

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -25,3 +25,40 @@ console.log(valueInNumber);*/
 // let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+//*******************************************operations **************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "teesha"
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1"+2) //complex situation
+// console.log(1 +"2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log("1" + 2 + "2")
+
+
+//confusion code
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
