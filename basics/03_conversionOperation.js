92c63fa3e19a53dd491cf346a498c71a1fcaf75b
teesha <debnathteesha51@gmail.com>
2025-04-04 06:10:52 +0000

    conversion

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+/*let score = undefined
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);*/
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false =>0 
+
+// let isLoggedIn = "teesha"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "teesha" => true
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
