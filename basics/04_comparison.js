7f7e61fef50572b26ff6fa9a008394a385b1aa9f
teesha <debnathteesha51@gmail.com>
2025-04-04 08:26:19 +0000

    new

---
+++ basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1) //basic comparisons
+
+//comparing that of different data types
+// console.log("2" > 1)
+// console.log("02" > 1) //this is not allowed in typescript
+
+//unpredictable results
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);//gives false
+
+
